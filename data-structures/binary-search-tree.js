795f6c98cb09fb98b122f768d61283bf2c8dcfb3
Lucia <luciaseggiaro@gmail.com>
2024-01-06 01:51:31 +0100

    1.5.1: Tree traversal, BFS and DFS

--- data-structures/binary-search-tree.js
+++ data-structures/binary-search-tree.js
@@ -24,7 +24,7 @@ class BinarySearchTree {
       return this;
     }
     let currentNode = this.root;
-    while (currentNode) {
+    while (true) {
       if (value === currentNode.value) return undefined; // Duplicated values are not allowed;
       if (value < currentNode.value) {
         if (currentNode.left === null) {
@@ -32,8 +32,7 @@ class BinarySearchTree {
           return this;
         }
         currentNode = currentNode.left;
-      }
-      if (value > currentNode.value) {
+      } else {
         if (currentNode.right === null) {
           currentNode.right = newNode;
           return this;
@@ -55,15 +54,76 @@ class BinarySearchTree {
     }
     return undefined;
   }
+
+  // TREEE TRAVERSAL VALID FOR ALL TREES, NOT ONLY BINARY SEARCH ONES
+
+  // Breath First Search
+  bfs() {
+    let node = this.root,
+      visited = [],
+      queue = [];
+    queue.push(node);
+
+    while (queue.length) {
+      node = queue.shift();
+      visited.push(node.value);
+      if (node.left) queue.push(node.left);
+      if (node.right) queue.push(node.right);
+    }
+    return visited;
+  }
+
+  // Depth First Search
+  dfsPreOrder() {
+    let visited = [];
+
+    function traverse(node) {
+      visited.push(node);
+      if (node.left) traverse(node.left);
+      if (node.right) traverse(node.right);
+    }
+
+    traverse(this.root);
+    return visited.map((node) => node.value);
+  }
+  dfsPostOrder() {
+    let visited = [];
+
+    function traverse(node) {
+      if (node.left) traverse(node.left);
+      if (node.right) traverse(node.right);
+      visited.push(node);
+    }
+
+    traverse(this.root);
+    return visited.map((node) => node.value);
+  }
+  dfsInOrder() {
+    let visited = [];
+
+    function traverse(node) {
+      if (node.left) traverse(node.left);
+      visited.push(node);
+      if (node.right) traverse(node.right);
+    }
+
+    traverse(this.root);
+    return visited.map((node) => node.value);
+  }
 }
+
+//        10
+//     6     15
+//   3  8      20
 
 let bst = new BinarySearchTree();
 bst.insert(10);
-bst.insert(5);
-bst.insert(13);
-bst.insert(11);
-bst.insert(2);
-bst.insert(16);
-bst.insert(7);
+bst.insert(6);
+bst.insert(15);
 bst.insert(3);
-console.log(bst.find(6));
+bst.insert(8);
+bst.insert(20);
+console.log(bst.bfs()); // [ 10, 6, 15, 3, 8, 20 ]
+console.log(bst.dfsPreOrder()); // [ 10, 6, 3, 8, 15, 20 ]
+console.log(bst.dfsPostOrder()); // [ 3, 8, 6, 20, 15, 10 ]
+console.log(bst.dfsInOrder()); // [ 3, 8, 6, 10, 15, 20 ]
