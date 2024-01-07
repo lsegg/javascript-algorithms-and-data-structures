90b2f947b11d45cb0f29628fc26905b578419286
Lucia <luciaseggiaro@gmail.com>
2024-01-07 01:07:23 +0100

    1.6.1: MBH extractMax

--- data-structures/binary-heaps.js
+++ data-structures/binary-heaps.js
@@ -6,15 +6,18 @@ class MaxBinaryHeap {
     this.values = [];
   }
 
-  bubbleUp(indexN = this.values.length - 1) {
-    const n = this.values[indexN];
-    const indexParent = Math.floor((indexN - 1) / 2);
-    const parent = this.values[indexParent];
+  bubbleUp(index = this.values.length - 1) {
+    const element = this.values[index];
 
-    if (n > parent) {
-      this.values[indexParent] = n;
-      this.values[indexN] = parent;
-      this.bubbleUp(indexParent);
+    while (index > 0) {
+      const parentIndex = Math.floor((index - 1) / 2); // Binary heap property
+      const parent = this.values[parentIndex];
+
+      if (element <= parent) break;
+
+      this.values[parentIndex] = element;
+      this.values[index] = parent;
+      index = parentIndex;
     }
   }
 
@@ -22,6 +25,50 @@ class MaxBinaryHeap {
     this.values.push(value);
     this.bubbleUp();
     return this.values;
+  }
+
+  sinkDown(indexParent = 0) {
+    const length = this.values.length;
+    let parent = this.values[indexParent];
+
+    while (true) {
+      const indexLeftChild = 2 * indexParent + 1; // Binary heap property
+      const indexRightChild = 2 * indexParent + 2; // Binary heap property
+      let indexToSwap = null;
+
+      if (indexLeftChild < length) {
+        const left = this.values[indexLeftChild];
+        if (left > parent) {
+          indexToSwap = indexLeftChild;
+        }
+      }
+
+      if (indexRightChild < length) {
+        const right = this.values[indexRightChild];
+        if (
+          (indexToSwap === null && right > parent) ||
+          (indexToSwap !== null && right > this.values[indexToSwap])
+        ) {
+          indexToSwap = indexRightChild;
+        }
+      }
+
+      if (indexToSwap === null) break;
+
+      this.values[indexParent] = this.values[indexToSwap];
+      this.values[indexToSwap] = parent;
+      indexParent = indexToSwap;
+    }
+  }
+
+  extractMax() {
+    const max = this.values[0];
+    const end = this.values.pop();
+    if (this.values.length > 0) {
+      this.values[0] = end;
+      this.sinkDown();
+    }
+    return max;
   }
 }
 
@@ -38,4 +85,5 @@ heap.values = [41, 39, 33, 18, 27, 12];
 heap.insert(55); // [55, 39, 41, 18, 27, 12, 33]
 heap.insert(1); // [55, 39, 41, 18, 27, 12, 33, 1]
 heap.insert(45); // [55, 45, 41, 39, 27, 12, 33, 1, 18];
+heap.extractMax(); // [45, 39, 41, 18, 27, 12, 33, 1];
 console.log(heap.values);
