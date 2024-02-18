fa1f5d1420b1c20aab6399817b57b3972eff2d0e
Lucia <luciaseggiaro@gmail.com>
2024-02-18 17:53:21 +0100

    1.8.1: graph traversal

--- data-structures/graphs.js
+++ data-structures/graphs.js
@@ -28,6 +28,70 @@ class Graph {
     this.adjacencyList[vertex].forEach((v) => this.removeEdge(v, vertex));
     delete this.adjacencyList[vertex];
   }
+
+  depthFirstRecursive(start) {
+    let results = [],
+      visited = {};
+    const adjacencyList = this.adjacencyList;
+
+    (function DFS(vertex) {
+      if (!vertex) return null;
+      visited[vertex] = true;
+      results.push(vertex);
+      adjacencyList[vertex].forEach((neighbour) => {
+        if (!visited[neighbour]) {
+          return DFS(neighbour);
+        }
+      });
+    })(start);
+    return results;
+  }
+
+  depthFirstIterative(start) {
+    let stack = [],
+      results = [],
+      visited = {},
+      currentVertex;
+    visited[start] = true;
+    stack.push(start);
+
+    while (stack.length) {
+      currentVertex = stack.pop();
+      results.push(currentVertex);
+      let neighbours = this.adjacencyList[currentVertex];
+
+      neighbours.forEach((neighbour) => {
+        if (!visited[neighbour]) {
+          visited[neighbour] = true;
+          stack.push(neighbour);
+        }
+      });
+    }
+    return results;
+  }
+
+  breadthFirst(start) {
+    let queue = [],
+      results = [],
+      visited = {},
+      currentVertex;
+    visited[start] = true;
+    queue.push(start);
+
+    while (queue.length) {
+      currentVertex = queue.shift();
+      results.push(currentVertex);
+      let neighbours = this.adjacencyList[currentVertex];
+
+      neighbours.forEach((neighbour) => {
+        if (!visited[neighbour]) {
+          visited[neighbour] = true;
+          queue.push(neighbour);
+        }
+      });
+    }
+    return results;
+  }
 }
 
 let flights = new Graph();
@@ -45,3 +109,30 @@ flights.addEdge("Dallas", "Los Angeles");
 flights.addEdge("Hong Kong", "Los Angeles");
 flights.removeVertex("Hong Kong");
 console.log(flights.adjacencyList);
+
+//          A
+//        /   \
+//       B     C
+//       |     |
+//       D --- E
+//        \   /
+//          F
+
+let g = new Graph();
+g.addVertex("A");
+g.addVertex("B");
+g.addVertex("C");
+g.addVertex("D");
+g.addVertex("E");
+g.addVertex("F");
+g.addEdge("A", "B");
+g.addEdge("A", "C");
+g.addEdge("B", "D");
+g.addEdge("C", "E");
+g.addEdge("D", "E");
+g.addEdge("D", "F");
+g.addEdge("E", "F");
+console.log(g.adjacencyList);
+console.log(g.depthFirstRecursive("A"));
+console.log(g.depthFirstIterative("A"));
+console.log(g.breadthFirst("A"));
